e566bb0bba57b71054195cff028f35e0883ce585
Daniela Paiva <danielap@neuronaagency.com>
2024-09-10 10:50:32 -0500

    add origin

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://amplify.d312qx2bvo4emp.amplifyapp.com',
   output: "server",
   adapter: awsAmplify()
 });
