13fd1f3dea6339a88e222efe80e4ce06e52d6e9a
Daniela Paiva <danielap@neuronaagency.com>
2024-09-25 20:20:08 -0500

    change sendmail function location

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,5 @@
 import { ActionError, defineAction } from "astro:actions";
 import { z } from "astro:content";
-import { SendMail } from "../../mailersend";
 import { TestDriveEmail } from "../emails";
 
 export const server = {
@@ -21,4 +20,53 @@ export const server = {
         }
     },
 })
+}
+
+export const SendMail = async (subject: string, html: string) => {
+  let url = 'https://api.mailersend.com/v1/email';
+  const apiKey = import.meta.env.MAILERSEND_API_KEY;
+  const fromEmmail = import.meta.env.MAIL_FROM_EMAIL;
+  const fromName = import.meta.env.MAIL_FROM_NAME;
+  const toEmmail = import.meta.env.MAIL_TO_EMAIL;
+  const toName = import.meta.env.MAIL_TO_NAME;
+
+  const body = {
+      from: {
+        email: fromEmmail,
+        name: fromName
+      },
+      to: [
+        {
+          email: toEmmail,
+          name: toName
+        }
+      ],
+      subject: subject,
+      text: subject,
+      html: html
+    };
+
+  let options = {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (response.ok) {
+      console.log('Email sent successfully:');
+      return true;
+    } else {
+      const result = await response.json();
+      console.error('Failed to send email:', result.errors);
+      return result.errors;
+    }
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return error;
+  }
 }
