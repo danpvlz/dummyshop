0a71e8fef86bf3497308e1ea9068bc9b7a44edad
Daniela Paiva <danielapv200@gmail.com>
2024-10-08 16:27:53 -0500

    redo pagefind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import awsAmplify from 'astro-aws-amplify';
 
-import pagefind from "astro-pagefind";
-
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   outDir: "./.amplify-hosting",
   adapter: awsAmplify(),
-  integrations: [pagefind()]
 });
