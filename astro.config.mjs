ba7778a8fc3992e3d2c737b8b71aa6ea6c08f871
Daniela Paiva <danielapv200@gmail.com>
2024-10-08 11:33:20 -0500

    Test search component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import awsAmplify from 'astro-aws-amplify';
 
+import pagefind from "astro-pagefind";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: awsAmplify()
+  outDir: "./.amplify-hosting",
+  adapter: awsAmplify(),
+  integrations: [pagefind()]
 });
